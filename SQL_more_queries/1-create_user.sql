f340062f22737beb37d6323993d8f3e708a4f862
John-Shewa <eldi2002@gmail.com>
2023-08-15 04:19:27 -0700

    SQL

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- A script that creates a user user_0d_1 with all previlages
-CREATE USER
-IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILAGES ON *.* TO 'user_0d_1'@'localhost';
