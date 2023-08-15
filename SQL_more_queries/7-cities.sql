c4ce2972d078cbd7778b8d14dada24ea8e05c337
John-Shewa <eldi2002@gmail.com>
2023-08-15 06:22:47 -0700

    SQL

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,10 +1,9 @@
 -- A script that creates a database and a table in the database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL,
-	state_id INT NOT NULL, 
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id)
-	FOREIGN KEY (state_id) REFERENCES states(id)
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+	`id` INT UNIQUE AUTO_INCREMENT NOT NULL,
+	`state_id` INT NOT NULL, 
+	`name` VARCHAR(256) NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`)
 	);
