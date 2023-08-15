51a27edcd5fd5847d135d199897c2c340c96cbf7
Yohannes Shewakena <yohannessh@dbe.com>
2023-08-15 13:55:45 +0300

    SQL

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- A script that creates a user user_0d_1 with all previlages
-CREATE USER
-IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE OR REPLACE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILAGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PREVILAGES;
