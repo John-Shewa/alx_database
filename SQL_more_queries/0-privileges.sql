e883cd9581b1c0c7ad355e3e5902ad30f54ff1bf
Yohannes Shewakena <yohannessh@dbe.com>
2023-08-15 13:39:44 +0300

    SQL

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- A script that lists all previlages of MYSQL users
-SELECT
-    privilage
-WHERE
-    host = 'localhost';
+SHOW GRANTS
+FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
